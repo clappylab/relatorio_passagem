a8f77afa367c554787676bc80cf65fdfc698b353
Rodrigo Henriques <rodrigobhenriques@gmail.com>
2026-09-21 12:59:35 -0300

    Primeiro commit do projeto

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def pagina_inicial(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
